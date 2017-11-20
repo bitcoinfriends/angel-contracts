66053eda281b6af259650384b3d8225ba3d3de2c
bitcoinfriends <bitcoinfriends@outlook.com>
2017-11-20 20:40:23 +0000

    change decimals to 18 back

--- contracts/angel/AngelToken.sol
+++ contracts/angel/AngelToken.sol
@@ -29,7 +29,7 @@ contract AngelToken is StandardToken, NamedToken, Pausable {
 
   /* Constructor */
 
-  function AngelToken() NamedToken('ANGEL', 'ANG', 8) {
+  function AngelToken() NamedToken('ANGEL', 'ANG', 18) {
     centralBankAddress = msg.sender;
   }
 
